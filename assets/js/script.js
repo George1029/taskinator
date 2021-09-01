3904c9899bffec3da1177e915bc52e1cfcbd813a
George Ellison <georgeellison1029@gmail.com>
2021-09-01 08:37:32 -0500

    refactored

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,33 @@
-var tasksToDoEl = document.querySelector("#tasks-to-do");
 var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function (event) {
+var taskFormHandler = function (event) {
     event.preventDefault();
+
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    var taskInfoEl = document.createElement("div");
-    taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-    listItemEl.appendChild(taskInfoEl);
-    tasksToDoEl.appendChild(listItemEl);
-    console.log(taskTypeInput);
-    console.dir(listItemEl);
-    //console.dir(taskNameInput);
-    //console.log(event);
+
+
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    createTaskEl(taskDataObj);
 
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = taskNameInput;
 }
+var createTaskEl = function (taskDataObj) {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
 
-formEl.addEventListener("submit", createTaskHandler);
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    tasksToDoEl.appendChild(listItemEl);
+}
+formEl.addEventListener("submit", taskFormHandler);
