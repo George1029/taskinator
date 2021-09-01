8be29a056510b2345ef37e6fa62881ec2cff1f2a
George Ellison <georgeellison1029@gmail.com>
2021-09-01 09:17:53 -0500

    added form reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,16 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskFormHandler = function (event) {
     event.preventDefault();
+   
 
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+    formEl.reset();
 
     var taskDataObj = {
         name: taskNameInput,
